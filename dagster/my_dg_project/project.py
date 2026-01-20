2d900c1e2425565fb20cb291664da0b4854a7dd8
Amir Jaber <38318312+Terroface@users.noreply.github.com>
2026-01-20 17:22:40 +0100

    feat: add generate_cli_args to DbtProject initialization for enhanced configuration

--- dagster/my_dg_project/project.py
+++ dagster/my_dg_project/project.py
@@ -1,8 +1,15 @@
+import os
 from pathlib import Path
 
 from dagster_dbt import DbtProject
 
 dbt_project = DbtProject(
     project_dir=Path(__file__).parent.parent.parent.joinpath("dbt").resolve(),
+    generate_cli_args=[
+        "parse",
+        "--quiet",
+        "--vars",
+        f'{{"is_non_python_interacting_dbt_asset_enabled": {os.getenv("IS_NON_PYTHON_INTERACTING_DBT_ASSET_ENABLED", "true").lower() == "true"}}}',
+    ]
 )
 dbt_project.prepare_if_dev()
